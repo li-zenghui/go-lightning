beeeefc8579991d9bd418d3dd2c273f943e0bff4
li3000 <lizenghui0827@163.com>
2021-10-19 19:55:25 +0800

    打印日志

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "go-lightning/server"
+import (
+	"fmt"
+	"go-lightning/server"
+)
 
 func main() {
-	S := server.NewServer("127.0.0.1", 7123)
+	S := server.NewServer("127.0.0.1", 8125)
 	S.Start()
+	fmt.Println("[go-lightning]start,port:8125")
 }
 
 // 上线 加载相关group 持久维持group连接
